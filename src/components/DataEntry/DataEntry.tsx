973441ed4e9347ce157e01d09d1fbbf4708ceb7d
Gee Chai <fc00004@gmail.com>
2024-03-18 14:42:18 +0900

    fix: values & keys format correctly

--- src/components/DataEntry/DataEntry.tsx
+++ src/components/DataEntry/DataEntry.tsx
@@ -5,9 +5,13 @@ const DataEntry = () => {
   const [textAreaValue, setTextAreaValue] = useState("");
   const [csvData, setCsvData] = useState("");
   const [keysStored, setKeysStored] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState(false); // New state to track loading
+  const [successMessage, setSuccessMessage] = useState("");
 
   const handleProcess = () => {
-    // Filter out lines starting with "#" in addition to empty lines
+    setIsLoading(true); // Start loading
+    setSuccessMessage(""); // Reset success message
+
     const lines = textAreaValue
       .split("\n")
       .filter((line) => line.trim() !== "" && !line.trim().startsWith("#"));
@@ -17,26 +21,35 @@ const DataEntry = () => {
 
     for (let i = 0; i < lines.length; i += 2) {
       const key = lines[i].replace(/^(\d+）)/, "").trim();
-      // Ensure lines[i + 1] exists before accessing it
       const value = lines[i + 1]
         ? lines[i + 1].trim() === "-"
           ? ""
           : lines[i + 1].trim()
         : "";
 
-      if (!keysStored.includes(key)) {
+      if (!csvData && !keysStored.includes(key)) {
+        // Add keys only if they're not already stored and it's the first batch
         keys.push(`"${key}"`);
+      }
+
+      if (!keysStored.includes(key)) {
         setKeysStored((prevKeys) => [...prevKeys, key]);
       }
       values.push(`"${value}"`);
     }
 
-    // Combine keys and values into CSV format
-    const newCsvContent = keys.length > 0 ? keys.join(",") + "\n" : "";
-    const csvContent = csvData
-      ? csvData + values.join(",") + "\n"
-      : keys.join(",") + "\n" + values.join(",") + "\n";
+    const valuesCsvContent = values.join(",") + "\n";
+    const csvContent =
+      !csvData && keys.length > 0
+        ? keys.join(",") + "\n" + valuesCsvContent
+        : csvData + valuesCsvContent;
+
     setCsvData(csvContent);
+    setTextAreaValue(""); // Clear the textarea
+    setIsLoading(false); // End loading
+    setSuccessMessage(
+      "Data processed successfully. Ready to add more or download.",
+    ); // Set success message
   };
 
   const handleDownload = () => {
@@ -55,9 +68,19 @@ const DataEntry = () => {
         className={styles.textArea}
         value={textAreaValue}
         onChange={(e) => setTextAreaValue(e.target.value)}
+        placeholder="Enter your data here..."
       ></textarea>
+      {isLoading && <p>Processing...</p>}
+      {successMessage && (
+        <p className={styles.successMessage}>{successMessage}</p>
+      )}
       <div>
-        <button className={styles.button} type="button" onClick={handleProcess}>
+        <button
+          className={styles.button}
+          type="button"
+          onClick={handleProcess}
+          disabled={!textAreaValue}
+        >
           Process
         </button>
         <button

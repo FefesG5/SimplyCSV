53ab0bc7bf0b6bd0815c93c50d5b5f42dfe474d9
Gee Chai <fc00004@gmail.com>
2024-03-25 13:50:23 +0900

    styles: center h1 text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import DataEntry from "@/components/DataEntry/DataEntry";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>Home Page</h1>
+      <h1 className={styles.heading}>Simply CSV</h1>
       <DataEntry />
     </div>
   );

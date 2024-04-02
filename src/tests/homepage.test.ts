bc90dc979b78e8da36465c94d712b82191b90014
Gee Chai <fc00004@gmail.com>
2024-04-02 22:01:29 +0900

    test: navigation links present in header check

--- src/tests/homepage.test.ts
+++ src/tests/homepage.test.ts
@@ -1,15 +1,26 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Header component test", () => {
+test.describe("Home Page test", () => {
   // Setup before each test
   test.beforeEach(async ({ page }) => {
     // Navigate to the page where the Header component is rendered
     await page.goto("http://localhost:3000");
   });
 
-  test("homepage has a title 'Simply CSV'", async ({ page }) => {
-    await page.goto("http://localhost:3000");
+  test("navigation links are present and correct", async ({ page }) => {
+    // Check for the existence of navigation links
+    await expect(page.locator("text=About")).toHaveAttribute("href", "/about");
+    await expect(page.locator("text=Administrator Access")).toHaveAttribute(
+      "href",
+      "/administrator-access",
+    );
+    await expect(page.locator("text=Contact")).toHaveAttribute(
+      "href",
+      "/contact",
+    );
+  });
 
+  test("homepage has a title 'Simply CSV'", async ({ page }) => {
     // Check for page title
     await expect(page).toHaveTitle(/Simply CSV/);
 

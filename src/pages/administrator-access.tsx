7dda3695ca7398954b0ce1390f9e9b46d7218ab7
Gee Chai <fc00004@gmail.com>
2024-04-09 13:17:36 +0900

    chore: import firebase into Administrator Access component

--- src/pages/administrator-access.tsx
+++ src/pages/administrator-access.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { getAuth, signOut } from "firebase/auth";
+
 const AdminstratorAccess: React.FC = () => {
   return <>Adminstrator Access Component</>;
 };

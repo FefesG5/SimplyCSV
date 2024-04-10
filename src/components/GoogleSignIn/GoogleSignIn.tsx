6a538db35d37786430e24eae2ad6e4b9d78744c5
Gee Chai <fc00004@gmail.com>
2024-04-10 09:46:26 +0900

    feat: GoogleSignIn Component added

---
+++ src/components/GoogleSignIn/GoogleSignIn.tsx
@@ -0,0 +1,46 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from "../../../firebaseConfig";
+import Image from "next/image";
+import styles from "./GoogleSignIn.module.css";
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+interface SignInProps {
+  setError: (error: string) => void;
+}
+
+const GoogleSignIn: React.FC<SignInProps> = ({ setError }) => {
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+        setError("Failed to sign in. Please try again.");
+      }
+    }
+  };
+
+  return (
+    <div className={styles.signInContainer}>
+      <p className={styles.signInText}>
+        Please sign in to access educator features
+      </p>
+      <button
+        className={styles.googleSignInButton}
+        onClick={signInWithGoogle}
+        aria-label="Sign in with Google"
+      >
+        <Image
+          src="/web_light_sq_SI.svg"
+          alt="Google logo"
+          width={183}
+          height={40}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default GoogleSignIn;

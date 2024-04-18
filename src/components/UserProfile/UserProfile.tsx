450c4cdeaf68d7dbacb18bf229c01dba05cdc5f5
Gee Chai <fc00004@gmail.com>
2024-04-18 13:07:44 +0900

    refactor: UserProfile Component created

---
+++ src/components/UserProfile/UserProfile.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import styles from "./UserProfile.module.css";
+
+interface UserProfileProps {
+  name: string;
+  photoUrl: string;
+}
+
+const UserProfile: React.FC<UserProfileProps> = ({ name, photoUrl }) => {
+  return (
+    <div className={styles.userProfileContainer}>
+      <div className={styles.userImageContainer}>
+        <Image
+          src={photoUrl || ""}
+          width={100}
+          height={100}
+          alt="User Profile"
+        />
+      </div>
+      <div className={styles.userName}>{name || "Guest"}</div>
+    </div>
+  );
+};
+
+export default UserProfile;

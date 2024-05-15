94312aad56217582d69f6af80013ed82bfd3ecc2
Gee Chai <fc00004@gmail.com>
2024-05-15 10:28:36 +0900

    feat: import DataEntry Component to administration page

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import styles from "./Dashboard.module.css";
 import UserProfile from "../UserProfile/UserProfile";
 import SignOutButton from "../SignOutButton/SignOutButton";
+import DataEntry from "../DataEntry/DataEntry";
 
 interface UserProfileProps {
   name: string;
@@ -15,7 +16,9 @@ interface DashboardProps {
 const Dashboard: React.FC<DashboardProps> = ({ userProfile, signOutUser }) => {
   return (
     <div className={styles.dashboardContainer}>
-      <div className={styles.mainSection}>Welcome to Dashboard</div>
+      <div className={styles.mainSection}>
+        <DataEntry />
+      </div>
       <div className={styles.userSection}>
         <UserProfile name={userProfile.name} photoUrl={userProfile.photoUrl} />
         <SignOutButton signOutUser={signOutUser} />

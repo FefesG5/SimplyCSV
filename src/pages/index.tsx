822b30e05e7e549d8b1c0dd3f50b7951e4eef876
Gee Chai <fc00004@gmail.com>
2024-05-15 14:02:21 +0900

    refactor: removed DataEntry component from main index page to protect the component from unauthorised use

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import styles from "./Home.module.css";
-import DataEntry from "@/components/DataEntry/DataEntry";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>Simply CSV</h1>
-      <DataEntry />
     </div>
   );
 }
